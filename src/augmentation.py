d2de16ea319f79f5606010c5055cf278a79f4938
akar <arkadiusz.karbowski@proton.me>
2025-03-17 18:58:11 +0100

    update albumentations dependency and refactor augmentation transforms

--- src/augmentation.py
+++ src/augmentation.py
@@ -2,45 +2,31 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 
-class NailAugmentation:
-    def __init__(self, mean, std, p=0.5):
-        self.transform = A.Compose(
-            [
-                A.OneOf(
-                    [
-                        A.RandomBrightnessContrast(p=1),
-                        A.ColorJitter(p=1),
-                        A.HueSaturationValue(p=1),
-                    ],
-                    p=0.5,
-                ),
-                A.OneOf(
-                    [
-                        A.GaussianBlur(p=1),
-                        A.MotionBlur(p=1),
-                        A.MedianBlur(p=1),
-                    ],
-                    p=0.3,
-                ),
-                A.OneOf(
-                    [
-                        A.ElasticTransform(p=1),
-                        A.GridDistortion(p=1),
-                        A.OpticalDistortion(p=1),
-                    ],
-                    p=0.2,
-                ),
-                A.OneOf(
-                    [
-                        A.RandomRotate90(p=1),
-                        A.HorizontalFlip(p=1),
-                        A.ShiftScaleRotate(p=1),
-                    ],
-                    p=0.5,
-                ),
-                A.Resize(224, 224),
-                A.Normalize(mean=mean, std=std),
-                ToTensorV2(),
-            ],
-            p=p,
-        )
+def get_transforms(dataset_stats):
+    transform_normal = A.Compose(
+        [
+            A.Resize(
+                dataset_stats["resize_shape"][0], dataset_stats["resize_shape"][1]
+            ),
+            A.Normalize(
+                mean=dataset_stats["mean_normal"],
+                std=dataset_stats["std_normal"],
+            ),
+            ToTensorV2(),
+        ]
+    )
+
+    transform_uv = A.Compose(
+        [
+            A.Resize(
+                dataset_stats["resize_shape"][0], dataset_stats["resize_shape"][1]
+            ),
+            A.Normalize(
+                mean=dataset_stats["mean_uv"],
+                std=dataset_stats["std_uv"],
+            ),
+            ToTensorV2(),
+        ]
+    )
+
+    return {"normal": transform_normal, "uv": transform_uv}
